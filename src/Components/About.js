1a9263766192cd9facf8d7b2df29c1e99dce3195
vivek <viveksagar7319@gmail.com>
2024-04-03 16:22:49 +0530

    about section chart work

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,16 +1,143 @@
-import React from "react";
-import bgImage2 from '../assets/bgImage2.png'
-import Chart from '../Design/Chart'
+import React, { useState } from "react";
+import bgImage2 from "../assets/bgImage2.png";
+import Chart from "../Design/Chart";
+import programming_icon from "../assets/programmig_icon.png";
+import tech_icon from "../assets/tech_icon.png";
+import core_icon from "../assets/core_icon.png";
+import tools_icon from "../assets/tools_icon.png";
+import skill_icon from "../assets/skill_icon.png";
+
 const About = () => {
+  const [activeSkill, setActiveSkill] = useState("programming");
+
+  const dataSet = {
+    technical: [
+      {
+        name: "React-JS",
+        color: "bg-blue-500",
+        percentage: "80%",
+      },
+      {
+        name: "JavaScript",
+        color: "dark:bg-yellow-500",
+        percentage: "90%",
+      },
+      {
+        name: "CSS",
+        color: "dark:bg-green-500",
+        percentage: "85%",
+      },
+      {
+        name: "HTML",
+        color: "dark:bg-orange-500",
+        percentage: "80%",
+      },
+      {
+        name: "Bootstrap",
+        color: "dark:bg-[#7912f9]",
+        percentage: "75%",
+      },
+      {
+        name: "Java",
+        color: "dark:bg-red-500",
+        percentage: "70%",
+      },
+    ],
+    programming: [
+      {
+        name: "Javascript",
+        color: "bg-yellow-400",
+        percentage: "80%",
+      },
+      {
+        name: "Java",
+        color: "dark:bg-orange-400",
+        percentage: "70%",
+      },
+    ],
+    Technologies: [
+      {
+        name: "React-Js",
+        color: "bg-blue-500",
+        percentage: "90%",
+      },
+      {
+        name: "Bootstrap",
+        color: "dark:bg-[#7912f9]",
+        percentage: "70%",
+      },
+      {
+        name: "Material-UI",
+        color: "dark:bg-[#007fff]",
+        percentage: "75%",
+      },
+      {
+        name: "Ant-UI",
+        color: "dark:bg-red-600",
+        percentage: "75%",
+      },
+      {
+        name: "Tailwind Css",
+        color: "dark:bg-[#07a2d2]",
+        percentage: "70%",
+      },
+    ],
+    Core: [
+      {
+        name: "React-Js",
+        color: "dark:bg-[#0f43a4]",
+        percentage: "90%",
+      },
+      {
+        name: "Material-UI",
+        color: "dark:bg-[#007fff]",
+        percentage: "75%",
+      },
+      {
+        name: "Bootstrap",
+        color: "dark:bg-[#7912f9]",
+        percentage: "70%",
+      },
+    ],
+    tools: [
+      {
+        name: "Figma",
+        color: "dark:bg-[#0f43a4]",
+        percentage: "80%",
+      },
+      {
+        name: "Git",
+        color: "dark:bg-orange-500",
+        percentage: "85%",
+      },
+      {
+        name: "Chrome",
+        color: "dark:bg-green-500",
+        percentage: "70%",
+      },
+      {
+        name: "Canva",
+        color: "dark:bg-blue-500",
+        percentage: "87%",
+      },
+      {
+        name: "Chatgpt",
+        color: "dark:bg-green-300",
+        percentage: "65%",
+      },
+    ],
+  };
   return (
     <div>
-
       <div className="z-50">
         <h1 className="text-yellow-400 text-center text-[3rem] underline tracking-widest z-50">
           ABOUT
         </h1>
         <div className="text-center flex justify-center z-50 ">
-      <img src={bgImage2} style={{position:"absolute",zIndex:"0",opacity:"0.7"}}></img>
+          <img
+            src={bgImage2}
+            style={{ position: "absolute", zIndex: "0", opacity: "0.7" }}
+          ></img>
           <p className="w-3/4 tracking-[0.2rem] text-white leading-10 text-[1.2rem] z-50">
             {" "}
             With one year of experience as a{" "}
@@ -26,39 +153,93 @@ const About = () => {
           </p>
         </div>
         <div className=" p-16 flex justify-between">
-
           <div
             className="border-solid w-1/4 bg-yellow-400/50 p-[5rem] flex flex-col gap-5 flex"
             style={{
-              background: " rgba(255, 244, 85,0.50)",
+              background: " rgba(0, 0, 0,0.2)",
               borderRadius: "16px",
               boxShadow: "0 0px 25px 2px #facc15",
-              backdropFilter: "blur(3px)",
+              backdropFilter: "blur(2px)",
               // -webkit-backdrop-filter: blur(3px);
               border: "1px solid rgba(255, 255, 255, 0.49)",
-
             }}
           >
+            <div
+              className={`transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 text-white ${
+                activeSkill == "technical" ? "bg-[#687EFF]" : null
+              } border flex p-5 rounded-md font-bold text-[1.3rem] items-center justify-center gap-2 cursor-pointer`}
+              onClick={() => setActiveSkill("technical")}
+            >
+              <div>
+                <img
+                  src={skill_icon}
+                  alt="programming-Icon"
+                  className="w-6"
+                />
+              </div>
+              <div>Technical Skills</div>
+            </div>
+            <div
+              className={`transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 text-white ${
+                activeSkill == "programming" ? "bg-[#687EFF]" : null
+              } border flex p-5 rounded-md font-bold text-[1.3rem] items-center justify-center gap-2 cursor-pointer`}
+              onClick={() => setActiveSkill("programming")}
+            >
+              <div>
+                <img
+                  src={programming_icon}
+                  alt="programming-Icon"
+                  className="w-6"
+                />
+              </div>
+              <div>Programming</div>
+            </div>
+            <div
+              className={`transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 text-white ${
+                activeSkill == "Technologies" ? "bg-[#687EFF]" : null
+              } border flex p-5 rounded-md font-bold text-[1.3rem] items-center justify-center gap-2 cursor-pointer`}
+              onClick={() => setActiveSkill("Technologies")}
+            >
+              <div>
+                <img src={tech_icon} alt="tech-Icon" className="w-6" />
+              </div>
+              <div>Technologies</div>
+            </div>
 
-              <div className="text-white bg-[#687EFF] text-center p-5 rounded-md font-bold text-[1.3rem]">Programming</div>
-              <div className="text-white border text-center p-5 rounded-md font-bold text-[1.3rem]">Technologies</div>
-            
-              <div className="text-white border text-center p-5 rounded-md font-bold text-[1.3rem]">Core CS</div>
-              <div className="text-white border text-center p-5 rounded-md font-bold text-[1.3rem]">Tools</div>
+            {/* <div
+              className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 text-white border flex p-5 rounded-md font-bold text-[1.3rem] items-center justify-center gap-2"
+              onClick={() => setActiveSkill("Core")}
+            >
+              <div>
+                <img src={core_icon} alt="core-Icon" className="w-6" />
+              </div>
+              <div>Core CS</div>
+            </div> */}
+            <div
+              className={`transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 text-white ${
+                activeSkill == "tools" ? "bg-[#687EFF]" : null
+              } border flex p-5 rounded-md font-bold text-[1.3rem] items-center justify-center gap-2 cursor-pointer`}
+              onClick={() => setActiveSkill("tools")}
+            >
+              <div>
+                <img src={tools_icon} alt="tools-Icon" className="w-6" />
+              </div>
+              <div>Tools</div>
+            </div>
           </div>
-          <div  className="border-solid w-2/4 bg-yellow-400/50 p-[5rem] flex flex-col gap-5 flex"
+          <div
+            className="border-solid w-2/4 bg-yellow-400/50 p-[5rem] flex flex-col gap-5 flex min-h-[600px]"
             style={{
-              background: " rgba(255, 244, 85,0.50)",
+              background: " rgba(0, 0, 0,0.2)",
               borderRadius: "16px",
               boxShadow: "0 0px 25px 2px #facc15",
               backdropFilter: "blur(3px)",
               // -webkit-backdrop-filter: blur(3px);
               border: "1px solid rgba(255, 255, 255, 0.49)",
-
-            }}>
-              <Chart/>
+            }}
+          >
+            <Chart dataSet={dataSet} activeSkill={activeSkill} />
           </div>
-          
         </div>
       </div>
     </div>
--- src/Design/Chart.js
+++ src/Design/Chart.js
@@ -1,83 +1,31 @@
-import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import React from "react";
 
-const chartSetting = {
-  xAxis: [
-    {
-      label: 'Skills (mm)',
-    },
-  ],
-  width: 800,
-  height: 400,
-};
-const dataset = [
-  {
-    london: 59,
-    paris: 57,
-    newYork: 86,
-    seoul: 21,
-    month: 'React',
-  },
- 
- 
- 
-  
-  {
-    london: 60,
-    paris: 63,
-    newYork: 103,
-    seoul: 144,
-    month: 'June',
-  },
-  {
-    london: 59,
-    paris: 60,
-    newYork: 105,
-    seoul: 319,
-    month: 'July',
-  },
-
-  {
-    london: 51,
-    paris: 51,
-    newYork: 95,
-    seoul: 131,
-    month: 'Sept',
-  },
-  {
-    london: 60,
-    paris: 65,
-    newYork: 97,
-    seoul: 55,
-    month: 'Oct',
-  },
-  {
-    london: 67,
-    paris: 64,
-    newYork: 76,
-    seoul: 48,
-    month: 'Nov',
-  },
-  {
-    london: 61,
-    paris: 70,
-    newYork: 103,
-    seoul: 25,
-    month: 'Dec',
-  },
-];
-const valueFormatter = (value) => `${value}mm`;
-
-export default function HorizontalBars() {
+const Chart = ({ dataSet, activeSkill }) => {
+  console.log(dataSet);
   return (
-    <BarChart
-      dataset={dataset}
-      yAxis={[{ scaleType: 'band', dataKey: 'month' }]}
-      series={[{ dataKey: 'seoul', label: 'Skill', valueFormatter }]}
-      layout="horizontal"
-      colors='#fff'
-      {...chartSetting}
-    
-    />
+    <>
+      {dataSet[`${activeSkill}`].map((item, index) => {
+        return (
+          <div>
+            <div className="flex justify-between">
+            <div className={`mb-1 text-base font-medium dark:text-white`}>
+              {item?.name}
+            </div>
+            <div className={`mb-1 text-base font-medium dark:text-white`}>
+              {item.percentage}
+            </div>
+            </div>
+            <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-400">
+              <div
+                className={`h-2.5 rounded-full ${item.color}`}
+                style={{ width: item?.percentage }}
+              ></div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
-}
+};
+
+export default Chart;
